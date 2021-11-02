5e9a5889a23f48e61551290f1495e4b4fe4b6b73
mengnan2024 <wufeianan2012@gmail.com>
2021-11-02 18:31:00 +0800

    excel 数据驱动

--- modeAPI/case/test_order_page.py
+++ modeAPI/case/test_order_page.py
@@ -1,32 +1,32 @@
-import datetime
-import datetime as dt
+import json
 
+import pytest
 import requests
 
 from modeAPI.utils.get_excel import readFromExcel
 
+file_path = r'C:\Users\Administrator\Desktop\order_api.xlsx'
+
 
 class TestOrderPage():
     '''先获取token， 传参'''
-    data = readFromExcel()
 
-    def test_AllOrder(self):
+    @pytest.mark.parametrize('row_num', [1, 2])
+    @pytest.mark.parametrize('sheet_name', ['订单相关'])
+    def test_AllOrder(self, sheet_name, row_num):
+        self.data = readFromExcel(file_path, sheet_name, row_num)
         url = self.data[3]
         headers = {
-            'authorization': 'eyJhbGciOiJIUzUxMiJ9.eyJsb2dpbl91c2VyX2tleSI6ImVkNWU4MGRjLTlkNDMtNDFjZi04ZGNkLTI5ODM0MmVhZDU2MSJ9.LhG8kCEq0IaBENPkswcIPQ9V5iN83RiZOBePT7ezYxw4kx83qRJdVFsqxgjcUQ90fo1kkRHaffRAD8JvLKdxWg'
+            'authorization': 'eyJhbGciOiJIUzUxMiJ9.eyJsb2dpbl91c2VyX2tleSI6ImI4ZTI3N2JkLThkNjAtNDkxOC04MGJkLTdjNTM3MjY1YzIyZiJ9.GTZtBclRmgAzTWwhjppHSEMjqdhV2ffGSuTBWsWC6e5Dr1KHHPo2oSzo2e9bnHJ6CG0TABRdqMZB2wi0xfzwng'
         }
 
-        params = {
-            'associateSplit': '1',
-            'fromTime': '2021-10-25 00:00:00',
-            'endTime': '2021-11-01 23:59:59',
-            'pageSize': '500',
-            'pageNum': '1',
-            'status': '0'
-        }
-        res = requests.post(url=url, params=params, headers=headers)
+        params = json.loads(self.data[6])
 
+        if self.data[4] == 'post':
+            res = requests.post(url=url, params=params, headers=headers)
+        if self.data[4] == 'get':
+            res = requests.get(url=url,headers=headers)
         assert res.status_code == 200
-        assert res.json()['code'] == 0
-        assert res.json()['message'] == 'successful'
-        assert res.json()['data'][0]['orderNum'] == 227
+        assert res.json()['code'] == self.data[7]
+        assert res.json()['message'] == self.data[9]
+        assert res.json()['data'][0]['orderNum'] == self.data[10]
--- modeAPI/utils/get_excel.py
+++ modeAPI/utils/get_excel.py
@@ -3,8 +3,7 @@ import pandas as pd
 
 '''读取'''
 
-def readFromExcel():
-    file_path = r'C:\Users\Administrator\Desktop\新建 XLSX 工作表.xlsx'
-    df = pd.read_excel(file_path, sheet_name='订单相关')
-    result = df.values[4].tolist()
+def readFromExcel(file_path, sheet_name ,row_num):
+    df = pd.read_excel(file_path, sheet_name)
+    result = df.values[row_num].tolist()
     return result
